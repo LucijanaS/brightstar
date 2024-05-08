444cd9e4c2dcce4970ce8a9cb1e47e85c050fb0f
LucijanaS <100121836+LucijanaS@users.noreply.github.com>
2024-05-08 10:42:09 +0200

    Working version, cross checked

--- fourier/example.py
+++ fourier/example.py
@@ -8,21 +8,21 @@ from graphics import draw
 
 pl.style.use('dark_background')
 
-N = 2048
-ds = 1e-10
-lam = 500e-9
+N = 2048  # N is grid size (equal on ground and sky)
+ds = 1e-10  # ds is grid spacing on the sky
 
-sx,sy,x,y = grids(ds,N,lam)
+lam = 500e-9 # wavelegnth in meters
+
+sx, sy, x, y = grids(ds, N, lam)
 
 Teff = 1e4
-Tmap = Teff * blob(sx,sy,3e-9)
-Tmap = smooth(Tmap,3)
-draw(sx,sy,Tmap,16,'sky',title='$T_{\\rm eff}$')
+Tmap = Teff * blob(sx, sy, 3e-9)
+Tmap = smooth(Tmap, 3)
+draw(sx, sy, Tmap, 16, 'sky', title='$T_{\\rm eff}$')
 pl.show()
 
-S = sbright(Tmap,lam,ds)
-f = correldens(S,lam)
+S = sbright(Tmap, lam, ds)
+f = correldens(S, lam)
 
-draw(x,y,f,16,'ground',cmap='coolwarm',title='$\Phi\,|V|^2$')
+draw(x, y, f, 16, 'ground', cmap='coolwarm', title='$\Phi\,|V|^2$')
 pl.show()
-
